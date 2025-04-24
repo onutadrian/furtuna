1294f5084d6a2833c88d7c8c0e78336e404c4460
Vicentiu-X <vicentiu@furtuna.ro>
2025-04-24 08:56:30 +0200

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -6,18 +6,32 @@ function sleep(ms) {
 
 function homeProjectsList() {
     let projectItem = document.getElementsByClassName(`_project-item`)
+    let lastProjectItem;
+
 
     for (let i = 0; i < projectItem.length; i++) {
 
         let mouseIsIn = false;
+        let videoId = projectItem[i].getAttribute(`video-id`)
+        let videoElement = document.getElementById(videoId)
 
         let projectTags = projectItem[i].getElementsByClassName(`_tag`)
         let gsapHover = projectItem[i].getElementsByClassName(`gsapHover`)[0]
+        let projectTitle = projectItem[i].getElementsByClassName(`_title`)[0]
         let chatWrapper = projectItem[i].getElementsByClassName(`_chat-wrapper`)[0]
 
-        gsapHover.addEventListener(`mouseenter`, async () => {
+        projectItem[i].addEventListener(`mouseenter`, async () => {
             if (mouseIsIn == false) {
                 mouseIsIn = true;
+
+                gsap.to(videoElement, {
+                    display: `block`,
+                    autoAlpha: .5,
+                    duration: 0.3
+                })
+
+                videoElement.play();
+
                 gsap.fromTo(Array.from(projectTags), {
                     y: -10,
                     opacity: 0
@@ -27,6 +41,11 @@ function homeProjectsList() {
                     opacity: 1,
                     duration: 0.2
                 });
+
+                gsap.to(projectTitle, {
+                    autoAlpha: 1,
+                    duration: 0.3
+                })
 
                 gsap.fromTo(chatWrapper, {
                     x: -32,
@@ -41,12 +60,26 @@ function homeProjectsList() {
 
         projectItem[i].addEventListener(`mouseleave`, async () => {
             mouseIsIn = false;
+
+            gsap.to(videoElement, {
+                display: `none`,
+                autoAlpha: 0,
+                duration: 0.3
+            })
+
+            videoElement.pause();
+
             gsap.to(Array.from(projectTags), {
                 y: -10,
                 stagger: 0.05,
                 opacity: 0,
                 duration: 0.2
             });
+
+            gsap.to(projectTitle, {
+                autoAlpha: 0.3,
+                duration: 0.3
+            })
 
             gsap.to(chatWrapper, {
                 x: -32,
